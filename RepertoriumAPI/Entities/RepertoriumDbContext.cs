67c1c76e53d231c9be4b92d583dd9806a4dc312f
KaroIex <karolkula11@gmail.com>
2023-03-10 03:37:32 +0100

    add first entities

---
+++ RepertoriumAPI/Entities/RepertoriumDbContext.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace RepertoriumAPI.Entities;
+
+public class RepertoriumDbContext : DbContext
+{
+    public RepertoriumDbContext(DbContextOptions<RepertoriumDbContext> options) : base(options) { }
+    
+    public DbSet<Advertisement> Advertisements { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Image> Images { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+       
+       
+        modelBuilder.Entity<Advertisement>()
+            .Property(a => a.Title)
+            .IsRequired()
+            .HasMaxLength(40);
+        modelBuilder.Entity<Advertisement>()
+            .Property(a => a.Description)
+            .IsRequired();
+
+        
+        modelBuilder.Entity<Category>()
+            .Property(a => a.Name)
+            .IsRequired()
+            .HasMaxLength(40);
+
+    }
+}
